ccb26660c51005022e5678310be22d3035147773
Iroman14 <134985621+Iroman14@users.noreply.github.com>
2024-12-16 03:56:03 +0000

    Trabajo Final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+const button = document.querySelector("#Execute");
+
+button.addEventListener("click", number);
+
+function number() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  function element(group) {
+    let RanNumber = Math.floor(Math.random() * group.length);
+    return group[RanNumber];
+  }
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  console.log(element(who) + element(action) + element(what) + element(when));
+  document.querySelector("#excuse").innerHTML =
+    element(who) + element(action) + element(what) + element(when);
+}
